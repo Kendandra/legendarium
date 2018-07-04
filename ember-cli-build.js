662ac1e4b89bc268f40a4b5a89da190aab489c79
Nathan Rojas <Metroid386@gmail.com>
2018-07-04 17:27:58 -0400

    added a sidebar

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,7 +6,12 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
     sassOptions: {
-      extension: 'sass'
+      extension: 'scss'
+    },
+    favicons: {
+      faviconsConfig: {
+
+      }
     }
   });
 
